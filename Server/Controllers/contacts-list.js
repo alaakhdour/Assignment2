536c363d3b070b3cd9a01351690747976ba3191a
alaakhdour <aalkhdou@my.centennialcollege.ca>
2022-06-20 19:21:44 -0400

    start assignment

---
+++ Server/Controllers/contacts-list.js
@@ -0,0 +1,79 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProcessDeletePage = exports.ProcessEditPage = exports.ProcessAddPage = exports.DisplayEditPage = exports.DisplayAddPage = exports.DisplayContactListPage = void 0;
+const contact_1 = __importDefault(require("../Models/contact"));
+const Util_1 = require("../Util");
+function DisplayContactListPage(req, res, next) {
+    contact_1.default.find(function (err, contactsCollection) {
+        if (err) {
+            console.error(err.message);
+            res.end(err);
+        }
+        res.render('index', { title: 'Contact List', page: 'contacts-list', contacts: contactsCollection, displayName: (0, Util_1.UserDisplayName)(req) });
+    });
+}
+exports.DisplayContactListPage = DisplayContactListPage;
+function DisplayAddPage(req, res, next) {
+    res.render('index', { title: 'Add', page: 'edit', contact: '', displayName: (0, Util_1.UserDisplayName)(req) });
+}
+exports.DisplayAddPage = DisplayAddPage;
+function DisplayEditPage(req, res, next) {
+    let id = req.params.id;
+    contact_1.default.findById(id, {}, {}, function (err, contactToEdit) {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.render('index', { title: 'Edit', page: 'edit', contact: contactToEdit, displayName: (0, Util_1.UserDisplayName)(req) });
+    });
+}
+exports.DisplayEditPage = DisplayEditPage;
+function ProcessAddPage(req, res, next) {
+    let newContact = new contact_1.default({
+        "Name": req.body.contactName,
+        "PhoneNumber": req.body.contactDirector,
+        "Email": req.body.contactYear,
+        "Rating": req.body.contactRating
+    });
+    contact_1.default.create(newContact, function (err) {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/contacts-list');
+    });
+}
+exports.ProcessAddPage = ProcessAddPage;
+function ProcessEditPage(req, res, next) {
+    let id = req.params.id;
+    let updatedContact = new contact_1.default({
+        "_id": id,
+        "Name": req.body.contactName,
+        "PhoneNumber": req.body.contactDirector,
+        "Email": req.body.contactYear,
+        "Rating": req.body.contactRating
+    });
+    contact_1.default.updateOne({ _id: id }, updatedContact, function (err) {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/contacts-list');
+    });
+}
+exports.ProcessEditPage = ProcessEditPage;
+function ProcessDeletePage(req, res, next) {
+    let id = req.params.id;
+    contact_1.default.remove({ _id: id }, function (err) {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/contacts-list');
+    });
+}
+exports.ProcessDeletePage = ProcessDeletePage;
+//# sourceMappingURL=contacts-list.js.map
