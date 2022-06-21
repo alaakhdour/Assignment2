820009825870c5ccebb963aea01323e78155a2a5
alaakhdour <aalkhdou@my.centennialcollege.ca>
2022-06-20 20:52:55 -0400

    try solve app crashed

---
+++ .history/Server/Config/db_20220620203742.ts
@@ -0,0 +1,6 @@
+export const LocalURI = "mongodb://127.0.0.1/media";
+export const RemoteURI = "mongodb+srv://Alaa:lNEftB4GaOcrYUFv@cluster0.phnn7ja.mongodb.net/media?retryWrites=true&w=majority";
+
+//export const RemoteURI = process.env.RemoteURI;
+export const HostName = "MongoDB Atlas";
+export const Secret = "someSecret";
